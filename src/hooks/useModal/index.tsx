f0fc4f3dcb27555f389c034fafb398e1c3a60bb9
Javier Daniel Gomez <daniel.gomez.1213@gmail.com>
2022-10-16 20:42:48 -0500

    disableScroll added

--- src/hooks/useModal/index.tsx
+++ src/hooks/useModal/index.tsx
@@ -1,9 +1,16 @@
 import { useState } from "react";
+import disableScroll from 'disable-scroll';
+
 
 export default function useModal() {
   const [isOpen, setisOpen] = useState(false);
 
   const toggle = () => {
+    if (!isOpen) {
+      disableScroll.on(); 
+    }else{
+      disableScroll.off();
+    }
     setisOpen(!isOpen);
   };
 
